f80720d5ab655fce5ca7d9371d7e9287a05c69f5
Pieter Frenssen <pieter@frenssen.be>
2019-06-26 08:52:23 +0300

    Add a service with helper methods for dealing with service tickets.

---
+++ src/ServiceTicketHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\cas_mock_server;
+
+/**
+ * Contains helper methods related to CAS service tickets.
+ */
+class ServiceTicketHelper {
+
+  /**
+   * Returns a service ticket.
+   *
+   * @return string
+   */
+  public static function generateServiceTicket(): string {
+    /** @var \Drupal\Component\Uuid\UuidInterface $uuid_service */
+    $uuid_service = \Drupal::service('uuid');
+    return 'ST-' . $uuid_service->generate();
+  }
+
+}
